573ffb3c2b3455a2be9cef52ecb7f642e278b4aa
Beau Vandervort <beauvandervort55@gmail.com>
2022-10-25 16:20:35 -0700

    Validated console log of name input

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const nameDisplay = document.getElementById('name');
+console.log('nameDisplay', nameDisplay);
+
+nameDisplay.textContent = 'Beau';
+nameDisplay.classList.add('pink');
+
 const updateBtn = document.getElementById('update-btn');
 updateBtn.addEventListener('click', () => {
     console.log('button was clicked');
