5962472d357b84744111f31e7740dfa6bca867cb
Kenni Woodard <kennikristine@gmail.com>
2022-10-25 16:47:26 -0700

    color selector is functional

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ updateBtn.addEventListener('click', () => {
     const nameInput = document.getElementById('name-input');
     nameDisplay.textContent = nameInput.value;
 });
+
+const colorDropdown = document.getElementById('color-dropdown');
+colorDropdown.addEventListener('change', () => {
+    console.log('menu was accessed');
+    console.log(colorDropdown.value);
+
+    const nameTagSection = document.getElementById('name-tag-section');
+    nameDisplay.classList.remove('orange');
+    nameDisplay.classList.remove('blue');
+    nameDisplay.classList.remove('yellow');
+    nameDisplay.classList.add(colorDropdown.value);
+});
